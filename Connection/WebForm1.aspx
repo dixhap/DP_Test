27748bfef62c1d7721b711b99afa376848e6fa6d
Deeksha Parsediya <dparsediya@Nerium.com>
2018-04-24 18:23:54 +0530

    add files

---
+++ Connection/WebForm1.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Connection.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td>
+                Name:
+            </td>
+            <td>
+               <asp:TextBox ID="TextName" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Email:
+            </td>
+            <td>
+               <asp:TextBox ID="TextEmail" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="Btn_Submit" runat="server" Text="Click" OnClick="Btn_Submit_Click" />
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblmesssage" runat="server"></asp:Label>
+            </td>
+        </tr>
+    </table>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Connection/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Connection
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string ConnectionString = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+               
+                SqlCommand cmd = new SqlCommand("Select *from employeeTB", connection);
+                connection.Open();
+                
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Btn_Submit_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_employeeTB", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@name", TextName.Text.Trim());
+                cmd.Parameters.AddWithValue("@email", TextEmail.Text.Trim());
+                connection.Open();
+                int i = cmd.ExecuteNonQuery();
+                if(i==1)
+                {
+                    TextName.Text = "";
+                    TextEmail.Text = "";
+                    lblmesssage.Text = "Record is saved";
+
+                }
+                else
+                {
+
+                }
+
+            }
+           
+        }
+    }
+}
